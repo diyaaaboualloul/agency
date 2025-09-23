a960225c104dec07d7f48ba132106d048016869a
diyaaaboualloul <diyaaaboualloul@gmail.com>
2025-09-23 15:41:30 +0300

    iconsite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
 <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+<link rel="icon" href="{{ asset('iconnnnn.png') }}" type="image/png">
 
         <!-- Scripts -->
     </head>
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -7,8 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title', 'Aximo || Responsive HTML 5 Template')</title>
 
-  <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}" type="image/x-icon">
-  <link rel="icon" href="{{ asset('assets/images/favicon.ico') }}" type="image/x-icon">
+<link rel="icon" href="{{ asset('iconnnnn.png') }}" type="image/png">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css2?family=Arimo:wght@400;700&family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet">
